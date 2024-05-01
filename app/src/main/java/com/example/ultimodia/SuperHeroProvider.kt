750ea142e0efdac5ae1f624d87eed73d8580291d
Elvis Tezen <elvistezenv@gmail.com>
2024-04-30 22:26:08 -0500

    first commit

---
+++ app/src/main/java/com/example/ultimodia/SuperHeroProvider.kt
@@ -0,0 +1,72 @@
+package com.example.ultimodia
+
+class SuperHeroProvider {
+    companion object {
+        val superHeroList = listOf<SuperHero> (
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+            SuperHero(
+                "Elvis",
+                "Arturo",
+                "Tezen",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbQCWugJXzW34hpyOBqLC-Ab86GCanA-6xxkOFRZ0Tg&s"
+            ),
+
+            )
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/ultimodia/adapter/SuperHeroViewHolder.kt
@@ -0,0 +1,25 @@
+package com.example.ultimodia.adapter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ultimodia.R
+import com.example.ultimodia.SuperHero
+import com.bumptech.glide.Glide
+
+class SuperHeroViewHolder (view : View) : RecyclerView.ViewHolder(view) {
+
+    val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
+    val realName = view.findViewById<TextView>(R.id.tvRealName)
+    val publisher = view.findViewById<TextView>(R.id.tvPublisher)
+    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+
+    fun render (superHeroModel: SuperHero){
+        superHero.text = superHeroModel.superHero
+        realName.text = superHeroModel.realName
+        publisher.text = superHeroModel.publisher
+        Glide.with(photo.context).load(superHeroModel.photo).into(photo);
+    }
+
+}
